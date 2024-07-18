a282ca4e4720817108f49edc9b6b767c1333bc84
namaek2 <science4588@gmail.com>
2024-07-19 04:45:52 +0900

    tested

---
+++ target/generated-sources/annotations/org/sample/jmh_generated/forObfTest_jmhType.java
@@ -0,0 +1,4 @@
+package org.sample.jmh_generated;
+public class forObfTest_jmhType extends forObfTest_jmhType_B3 {
+}
+
---
+++ target/generated-sources/annotations/org/sample/jmh_generated/forTest_jmhType.java
@@ -0,0 +1,4 @@
+package org.sample.jmh_generated;
+public class forTest_jmhType extends forTest_jmhType_B3 {
+}
+
