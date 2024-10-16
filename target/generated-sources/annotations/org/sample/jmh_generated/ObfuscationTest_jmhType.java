9eb22ad536f8588fc0cd2d54440482244f66c619
namaek2 <science4588@gmail.com>
2024-10-16 22:42:56 +0900

    build deleted

--- target/generated-sources/annotations/org/sample/jmh_generated/ObfuscationTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.sample.jmh_generated;
-public class ObfuscationTest_jmhType extends ObfuscationTest_jmhType_B3 {
-}
-
--- target/generated-sources/annotations/org/sample/jmh_generated/forTest_jmhType.java
+++
@@ -1,4 +0,0 @@
-package org.sample.jmh_generated;
-public class forTest_jmhType extends forTest_jmhType_B3 {
-}
-
